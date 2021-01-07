b710e493e121f63304e766b759bb5fc6398ddc1d
iOznny <fo170014@gmail.com>
2021-01-07 12:14:02 -0600

    Inicio routes

---
+++ src/app/app.routes.ts
@@ -0,0 +1,12 @@
+import { RouterModule, Routes } from '@angular/router';
+import { HomeComponent } from "./components/home/home.component";
+import { SearchComponent } from "./components/search/search.component";
+
+export const ROUTES: Routes = [
+    { path: 'home', component: HomeComponent },
+    { path: 'search', component: SearchComponent },
+    { path: '', pathMatch: 'full', redirectTo: 'home' },
+    { path: '**', pathMatch: 'full', redirectTo: 'home' }
+];
+
+export const app_routing = RouterModule.forRoot(ROUTES);
